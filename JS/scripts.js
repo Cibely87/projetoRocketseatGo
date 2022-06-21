76d3e8aaf4e0078370156402b87f1b055af69bd3
Cibely Pereira <cibely.port@gmail.com>
2022-06-21 15:09:38 -0300

    21/06

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,29 @@
 console.log('Lets Go!')
+
+/*CADEIA COM STRING
+"  " aspas duplas
+'  ' aspas simples
+`  ` templats literais*/
+
+
+/*NUMEROS
+33 numero inteiro
+12.2 numeros reais
+NaN NOT A NUMBER
+Infinity infiito */
+
+/* Undefined - indefinido;
+
+null - nulo;
+objeto que nao possui nada dentro;
+diferente de indefinido;*/
+
+/* Object;
+objeto / propriedades e atributos / funcionalidades e metodos 
+{ propriedade : "valor" }
+*/
+
+/* Array;
+uma lista / agrupamento de dados
+[ "Cibely", 34]
+*/
